5e7a651116aa12463fd5a40baa2f4939a0149759
Kustov Valery <valery.kustov@raiffeisen.ru>
2024-05-23 21:25:49 +0300

    refactored_code

--- wolf_assistant/handlers/message_handlers.py
+++ wolf_assistant/handlers/message_handlers.py
@@ -1,21 +1,28 @@
-from wolf_assistant.config.openai_client import client, generate_response
+"""Message Hanndler."""
+
+from loguru import logger
 from telegram import Update
+from telegram.ext import CallbackContext
+
+from wolf_assistant.clients.openai_client import generate_response
 
 
-async def chatgpt_reply(update: Update, context):
-    # текст входящего сообщения
-    text = update.message.text
+async def chatgpt_reply(update: Update, context: CallbackContext) -> None:
+    """Source Text reply from chatgpt.
 
-    # ответ
+    Args:
+        update (Update): Telegram object represented an incoming update.
+        context (ContextTypes.DEFAULT_TYPE): context object
+    """
+    if update.message and update.message.text:
+        text: str = update.message.text
+    else:
+        raise AttributeError("update.message is None")
+
+    logger.debug(f"Input message: {text}, Context: {context}")
+
     reply = generate_response(text)
+    logger.debug(f"Reply: {reply}")
 
     # перенаправление ответа в Telegram
     await update.message.reply_text(reply)
-
-    print("user:", text)
-    print("assistant:", reply)
-
-
-__all__ = [
-    'chatgpt_reply'
-]
